95fac7e86d71c6798b2fe994f271ebd84c1ec7a9
zip520123 <zip520123@yahoo.com.tw>
2018-12-12 16:09:42 +0800

    Updates

---
+++ precache-manifest.1c68748eb597a3d95581c79aab4deea9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "62e7b41121bf736c768224f79889a7d2",
+    "url": "./static/media/drtyWaterButton.62e7b411.png"
+  },
+  {
+    "revision": "ec555d4312952349c3df",
+    "url": "./static/css/main.6ad7f051.chunk.css"
+  },
+  {
+    "revision": "e22ebb95890a58d91d22f93c41ea1a75",
+    "url": "./static/media/waterKeepButton.e22ebb95.png"
+  },
+  {
+    "revision": "b680114a1dcd38965ede",
+    "url": "./static/js/1.b680114a.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "30a1257aa5e56f3d032db78b809b88ae",
+    "url": "./static/media/archButton.30a1257a.png"
+  },
+  {
+    "revision": "ec555d4312952349c3df",
+    "url": "./static/js/main.ec555d43.chunk.js"
+  },
+  {
+    "revision": "0fc9e8a8319d9001f7294532c4ff8b14",
+    "url": "./static/media/envEduButton.0fc9e8a8.png"
+  },
+  {
+    "revision": "16672daf254e149539c220e40404aa87",
+    "url": "./static/media/fileUseButton.16672daf.png"
+  },
+  {
+    "revision": "011296c32bebcfe0fae0b397811baa21",
+    "url": "./static/media/formSearchButton.011296c3.png"
+  },
+  {
+    "revision": "0d570d975f649cb720454f666485de87",
+    "url": "./static/media/waterKeepBack.0d570d97.png"
+  },
+  {
+    "revision": "b680114a1dcd38965ede",
+    "url": "./static/css/1.60a25e4e.chunk.css"
+  },
+  {
+    "revision": "dab7a58f8fe7b5d2ef2330b5c6a3e2b3",
+    "url": "./index.html"
+  }
+];
